6d8ff8b770b3270b5b750d2ff8ac1d7cbcdc8cb5
babayybay <147687642+babay162@users.noreply.github.com>
2025-04-05 22:29:35 +0700

    Update msm_drv.c

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -579,7 +579,8 @@ static int msm_drm_display_thread_create(struct sched_param param,
 		kthread_init_worker(&priv->event_thread[i].worker);
 		priv->event_thread[i].dev = ddev;
 		priv->event_thread[i].thread =
-			kthread_run(kthread_worker_fn,
+				kthread_run_perf_critical(cpu_prime_mask,
+ 				kthread_worker_fn,
 				&priv->event_thread[i].worker,
 				"crtc_event:%d", priv->event_thread[i].crtc_id);
 		/**
@@ -626,8 +627,8 @@ static int msm_drm_display_thread_create(struct sched_param param,
 	 * other important events.
 	 */
 	kthread_init_worker(&priv->pp_event_worker);
-	priv->pp_event_thread = kthread_run(kthread_worker_fn,
-			&priv->pp_event_worker, "pp_event");
+	priv->pp_event_thread = kthread_run_perf_critical(cpu_prime_mask,
+ 			kthread_worker_fn, &priv->pp_event_worker, "pp_event");
 
 	ret = sched_setscheduler(priv->pp_event_thread,
 						SCHED_FIFO, &param);
